9b748d0f3bd862bea20f210285ab29e565baef8c
stalinvirtual <44080520+stalinvirtual@users.noreply.github.com>
2023-12-23 18:28:21 +0530

    Update search_exam_year.php by stalin

--- dataentry/search_exam_year.php
+++ dataentry/search_exam_year.php
@@ -7,10 +7,24 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
     //get matched data 
     try {
         @$q = ($_GET['q']) ? $_GET['q'] : "";
-        $sql = "select distinct(table_exam_year) as year from sscsr_db_table_master   WHERE table_exam_year LIKE '%" . $q . "%'  ORDER BY table_exam_year DESC";
+       /* $sql = "select distinct(table_exam_year) as year from sscsr_db_table_master   WHERE table_exam_year LIKE '%" . $q . "%'  ORDER BY table_exam_year DESC";
         $stmt = $pdo->query($sql);
         $stmt->execute();
-        $result = $stmt->fetchAll();
+        $result = $stmt->fetchAll();*/
+        
+
+       // $q = ...; // Assume $q is defined and contains the search term
+
+// Using a prepared statement to prevent SQL injection
+$sql = "SELECT DISTINCT table_exam_year AS year FROM sscsr_db_table_master WHERE table_exam_year LIKE ? ORDER BY table_exam_year DESC";
+$stmt = $pdo->prepare($sql);
+
+// Binding the parameter and executing the statement
+$stmt->execute(["%" . $q . "%"]);
+
+// Fetching the result
+$result = $stmt->fetchAll();
+
         /*var_dump($sql);*/
 
     } catch (Exception $Ex) {
