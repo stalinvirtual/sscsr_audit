5a576b6dcfeb928ed04a77eff3b1803440c8c3d3
stalinvirtual <44080520+stalinvirtual@users.noreply.github.com>
2023-12-23 15:47:29 +0530

    Update exam_tier_master_publish_ajax.php

--- dataentry/exam_tier_master_publish_ajax.php
+++ dataentry/exam_tier_master_publish_ajax.php
@@ -12,10 +12,20 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
 
         date_default_timezone_set("Asia/Calcutta");
         $updated_time = $date = date("Y-m-d H:i:s");
-        $sql = "UPDATE public.sscsr_db_table_tier_master SET  no_of_days='$count_of_days',updated_on = '$updated_time' WHERE id='$id'";
+      /*  $sql = "UPDATE public.sscsr_db_table_tier_master SET  no_of_days='$count_of_days',updated_on = '$updated_time' WHERE id='$id'";
 
         $stmt = $pdo->prepare($sql);
-        $stmt->execute();
+        $stmt->execute();*/
+        $sql = "UPDATE public.sscsr_db_table_tier_master SET no_of_days=?, updated_on=? WHERE id=?";
+$stmt = $pdo->prepare($sql);
+
+
+$stmt->bindParam(1, $count_of_days, PDO::PARAM_INT);
+$stmt->bindParam(2, $updated_time, PDO::PARAM_STR);
+$stmt->bindParam(3, $id, PDO::PARAM_INT);
+
+$stmt->execute();
+
         $message = array(
             'response' => array(
                 'status' => 'success',
