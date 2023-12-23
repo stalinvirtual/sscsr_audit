c81366d616fe87bd5bfcec81f94a950798b58ba5
stalinvirtual <44080520+stalinvirtual@users.noreply.github.com>
2023-12-23 15:30:15 +0530

    Update Kyc_status_master_publish_ajax.php

--- dataentry/Kyc_status_master_publish_ajax.php
+++ dataentry/Kyc_status_master_publish_ajax.php
@@ -10,7 +10,7 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
     try {
         date_default_timezone_set("Asia/Calcutta");
         $updated_time = $date = date("Y-m-d H:i:s");
-        if ($iconid_id == "red") {
+       /* if ($iconid_id == "red") {
             $sql = "UPDATE public.sscsr_db_table_city_tier_master SET  status='1',updated_on = '$updated_time' WHERE id='$id'";
         } else {
             $sql = "UPDATE public.sscsr_db_table_city_tier_master SET  status='0',updated_on = '$updated_time' WHERE id='$id'";
@@ -18,7 +18,25 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
 
 
         $stmt = $pdo->prepare($sql);
-        $stmt->execute();
+        $stmt->execute();*/
+
+       // Assuming $iconid_id, $id, and $updated_time are properly validated and sanitized
+
+// Prepare the SQL statement with placeholders
+$sql = "UPDATE public.sscsr_db_table_city_tier_master SET status = :status, updated_on = :updated_time WHERE id = :id";
+
+// Set the value of status based on $iconid_id
+$status = ($iconid_id == "red") ? 1 : 0;
+
+// Prepare the parameters for binding
+$params = array('status' => $status, 'updated_time' => $updated_time, 'id' => $id);
+
+// Prepare and execute the statement
+$stmt = $pdo->prepare($sql);
+$stmt->execute($params);
+
+
+        
         $message = array(
             'response' => array(
                 'status' => 'success',
