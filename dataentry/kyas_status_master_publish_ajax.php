3c00d4caef17f5e506c35cf5dab04abd9468ace5
stalinvirtual <44080520+stalinvirtual@users.noreply.github.com>
2023-12-23 15:57:03 +0530

    Update kyas_status_master_publish_ajax.php. full file has changed

--- dataentry/kyas_status_master_publish_ajax.php
+++ dataentry/kyas_status_master_publish_ajax.php
@@ -1,50 +1,51 @@
 <?php
 require_once("config/db.php");
+
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest')) {
     require_once("functions.php");
+
     $id = $_POST['sscsr_db_table_master_id'];
     $iconid_id = $_POST['iconid'];
-    // Update user
 
+    // Update user
     try {
         date_default_timezone_set("Asia/Calcutta");
-        $updated_time = $date = date("Y-m-d H:i:s");
-        if ($iconid_id == "red") {
-            $sql = "UPDATE public.sscsr_db_table_master SET  status='1',updated_on = '$updated_time' WHERE table_id='$id'";
-        } else {
-            $sql = "UPDATE public.sscsr_db_table_master SET  status='0',updated_on = '$updated_time' WHERE table_id='$id'";
-        }
+        $updated_time = date("Y-m-d H:i:s");
+        $statusValue = ($iconid_id == "red") ? 1 : 0;
 
-
-		
+        // Use prepared statements for better security
+        $sql = "UPDATE public.sscsr_db_table_master SET status = ?, updated_on = ? WHERE table_id = ?";
         $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(1, $statusValue, PDO::PARAM_INT);
+        $stmt->bindParam(2, $updated_time, PDO::PARAM_STR);
+        $stmt->bindParam(3, $id, PDO::PARAM_INT);
+
         $stmt->execute();
+
         $message = array(
             'response' => array(
                 'status' => 'success',
                 'code' => '1',
-                // whatever you want
-                'message' => 'Know Your Status  Published Successfully.',
-                'title' => "Success"
+                'message' => 'Know Your Status Published Successfully.',
+                'title' => 'Success'
             )
         );
     } catch (Exception $e) {
-
+        // Log the error or use a more user-friendly error message
         $message = array(
             'response' => array(
                 'status' => 'error',
                 'code' => '0',
-                // whatever you want
-                'message' => $e->getMessage(),
+                'message' => 'An error occurred: ' . $e->getMessage(),
                 'title' => 'Error'
             )
         );
     }
-
-
 
     echo json_encode($message);
 } else {
     header("Location: index.php");
     exit();
 }
+?>
