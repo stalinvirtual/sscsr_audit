bafd25e8d6480c293206b4b5d3cda383240d05d0
stalinvirtual <44080520+stalinvirtual@users.noreply.github.com>
2023-12-23 18:26:22 +0530

    Update search_exam.php by stalin

--- dataentry/search_exam.php
+++ dataentry/search_exam.php
@@ -5,10 +5,25 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && ($_SERVER['HTTP_X_REQUESTED_WITH
     //get matched data 
     try {
         @$q = ($_GET['q']) ? $_GET['q'] : "";
-        $sql = "SELECT exam_name,exam_short_name from exam_master WHERE exam_name LIKE '%" . $q . "%'  ORDER BY exam_id DESC";
+       /* $sql = "SELECT exam_name,exam_short_name from exam_master WHERE exam_name LIKE '%" . $q . "%'  ORDER BY exam_id DESC";
         $stmt = $pdo->query($sql);
         $stmt->execute();
         $result = $stmt->fetchAll();
+
+        */
+
+        $q = ...; // Assume $q is defined and contains the search term
+
+// Using a prepared statement to prevent SQL injection
+$sql = "SELECT exam_name, exam_short_name FROM exam_master WHERE exam_name LIKE ? ORDER BY exam_id DESC";
+$stmt = $pdo->prepare($sql);
+
+// Binding the parameter and executing the statement
+$stmt->execute(["%" . $q . "%"]);
+
+// Fetching the result
+$result = $stmt->fetchAll();
+
         /*var_dump($sql);*/
 
     } catch (Exception $Ex) {
